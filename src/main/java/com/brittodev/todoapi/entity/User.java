b21eda462d6895941f8fb60e81bc3bb40908fa5f
britto_dev <brittoraj8888@gmail.com>
2026-05-15 23:55:00 +0530

    Stated to introduce Users for my project. Created a User Entity and UserRepository.

---
+++ src/main/java/com/brittodev/todoapi/entity/User.java
@@ -0,0 +1,24 @@
+package com.brittodev.todoapi.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+    private String email;
+    private String password;
+}
