5620fd587c2495acb7a11dbd2c609ff9e27188f9
britto_dev <brittoraj8888@gmail.com>
2026-05-15 23:38:31 +0530

    Implemented the ErrorResponse DTO for better understanding of the Exception.

--- src/main/java/com/brittodev/todoapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/brittodev/todoapi/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.brittodev.todoapi.exception;
 
 
+import com.brittodev.todoapi.dto.responseDto.error.ErrorResponse;
 import com.brittodev.todoapi.exception.custom.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -9,17 +11,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFound(
-            ResourceNotFoundException exception
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(
+            ResourceNotFoundException exception ,
+            HttpServletRequest servletRequest
     ) {
         log.atInfo().log("exception on resource not found");
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(exception.getMessage());
+                .body(ErrorResponse.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .status(HttpStatus.NOT_FOUND.value())
+                        .error("NOT FOUND")
+                        .message(exception.getMessage())
+                        .path(servletRequest.getRequestURI())
+                        .build());
     }
 }
